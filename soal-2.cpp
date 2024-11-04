3558d2725494bf9474f9fbfdf0b3e91746f270af
Lamy-lame <75929898+hyoga-dev@users.noreply.github.com>
2024-11-04 11:34:09 +0700

    Create soal-2.cpp

---
+++ soal-2.cpp
@@ -0,0 +1,29 @@
+#include <iostream> 
+using namespace std;
+
+void biayaParkir(int jumlahMotor, int jumlahMobil, int jamMasuk, int jamKeluar) {
+	int mobil = 5000;
+	int motor = 2000;
+	int jam;
+
+	if (jamMasuk < jamKeluar) {
+		jam = jamKeluar - jamMasuk;
+	} else {
+		jam = 24 - (jamMasuk - jamKeluar);
+	}
+	
+	int total = (jumlahMotor * motor * jam) + (jumlahMobil * mobil * jam);
+
+	cout << "\nTotal biaya parkir : " << total;
+	cout << " (" << jam << " jam)";
+}
+
+int main() {
+	int motor, mobil, masuk, keluar;
+
+	cout << "Masukkan nilai motor : "; cin >> motor;
+	cout << "Masukkan nilai mobil : "; cin >> mobil;
+	cout << "Input jam masuk (format 24 jam) : "; cin >> masuk;
+	cout << "Input jam keluar (format 24 jam) : "; cin >> keluar;
+	biayaParkir(motor, mobil, masuk, keluar);
+}
