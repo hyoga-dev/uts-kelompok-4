41a6acd7c270a36663a8d09dff56d8004fe3186d
Lamy-lame <75929898+hyoga-dev@users.noreply.github.com>
2024-11-04 17:29:54 +0700

    Update soal-2.cpp

--- soal-2.cpp
+++ soal-2.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-	int motor, mobil, masuk, keluar, hargamobil = 5000, hargamotor = 2000, jam;
+	int jumlahMotor, jumlahMobil, jamMasuk, jamKeluar, hargamobil = 5000, hargamotor = 2000, jam;
 
-	cout << "Masukkan nilai motor : "; cin >> motor;
-	cout << "Masukkan nilai mobil : "; cin >> mobil;
-	cout << "Input jam masuk (format 24 jam) : "; cin >> masuk;
-	cout << "Input jam keluar (format 24 jam) : "; cin >> keluar;
+	cout << "Masukkan nilai motor : "; cin >> jumlahMotor;
+	cout << "Masukkan nilai mobil : "; cin >> jumlahMobil;
+	cout << "Input jam masuk (format 24 jam) : "; cin >> jamMasuk;
+	cout << "Input jam keluar (format 24 jam) : "; cin >> jamKeluar;
 
 	(jamMasuk < jamKeluar) ? jam = jamKeluar - jamMasuk : jam = 24 - (jamMasuk - jamKeluar);
 	int total = (jumlahMotor * hargamotor * jam) + (jumlahMobil * hargamobil * jam);
